c3a5aa4a0a651d79fb144aa042618d79d2c95425
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-25 12:40:06 -0500

    On branch main 	modified:   Hollowrectangle.java

--- Hollowrectangle.java
+++ Hollowrectangle.java
@@ -13,7 +13,8 @@ public class Hollowrectangle {
             for (int j = 1; j <= col ; j++){
             if(i==1 || i==row || j==1 || j==col) System.out.print("* ");
             else System.out.print("  ");
-            }
+            }   
+         
             System.out.println();
         }
 
